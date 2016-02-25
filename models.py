78f0b2b835a977d4a93b82f191a4c4120edbc828
Ben Cail <benjamin_cail@brown.edu>
2016-02-25 16:31:43 -0500

    add created/modified fields to 4 of the dynamic models

--- models.py
+++ models.py
@@ -56,6 +56,8 @@ class Person(models.Model):
     address_state = models.CharField(max_length=2, blank=True)
     address_zip = models.CharField(max_length=20, blank=True)
     phone = models.CharField(max_length=50, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
@@ -75,6 +77,8 @@ class Candidate(models.Model):
     year = models.ForeignKey(Year)
     department = models.ForeignKey(Department)
     degree = models.ForeignKey(Degree)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return u'%s (%s)' % (self.person, self.year)
@@ -95,6 +99,8 @@ class CommitteeMember(models.Model):
     role = models.CharField(max_length=25, choices=MEMBER_ROLES, default=u'reader')
     department = models.ForeignKey(Department, null=True, blank=True)
     affiliation = models.CharField(max_length=190, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return u'%s (%s)' % (self.person, self.role)
@@ -156,6 +162,8 @@ class Thesis(models.Model):
     keywords = models.ManyToManyField(Keyword)
     language = models.ForeignKey(Language, null=True)
     status = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = u'Theses'
