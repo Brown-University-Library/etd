d8f16cf30fb404962fce6aeccb46a51c8b7e1c7a
Ben Cail <benjamin_cail@brown.edu>
2016-04-07 11:04:52 -0400

    update keyword widget to be able to handle new FAST values

--- test_widgets.py
+++ test_widgets.py
@@ -2,7 +2,7 @@
 from django.http import QueryDict
 from django.test import TestCase
 from .models import Keyword
-from .widgets import KeywordSelect2TagWidget
+from .widgets import KeywordSelect2TagWidget, ID_VAL_SEPARATOR, FAST_URI
 from .test_models import COMPOSED_TEXT, DECOMPOSED_TEXT
 
 
@@ -34,3 +34,17 @@ class TestKeywordTagWidget(TestCase):
         widget = KeywordSelect2TagWidget()
         value_from_datadict = widget.value_from_datadict(data, files, field_name)
         self.assertEqual(Keyword.objects.all()[0].id, int(value_from_datadict[0]))
+
+    def test_fast_keyword(self):
+        kw_text = Keyword.normalize_text(u'Lîon')
+        post_string = u'keywords=fst123456%s%s' % (ID_VAL_SEPARATOR, kw_text)
+        data = QueryDict(post_string.encode('utf8'))
+        files = {}
+        field_name = 'keywords'
+        widget = KeywordSelect2TagWidget()
+        value_from_datadict = widget.value_from_datadict(data, files, field_name)
+        new_keyword = Keyword.objects.all()[0]
+        self.assertEqual(new_keyword.text, kw_text)
+        self.assertEqual(new_keyword.authority, 'fast')
+        self.assertEqual(new_keyword.authority_uri, FAST_URI)
+        self.assertEqual(new_keyword.value_uri, u'%s/123456' % FAST_URI)
--- widgets.py
+++ widgets.py
@@ -3,9 +3,28 @@ from django_select2.forms import ModelSelect2TagWidget
 from .models import Keyword
 
 
+ID_VAL_SEPARATOR = u'\t'
+FAST_URI = u'http://id.worldcat.org/fast'
+
+
 class KeywordSelect2TagWidget(ModelSelect2TagWidget):
 
     queryset = Keyword.objects.all()
+
+    def _get_value_text(self, value):
+        #from the widget value, get the string that would go in the Keyword.text field
+        if ID_VAL_SEPARATOR in value:
+            value = value.split(ID_VAL_SEPARATOR, 1)[1]
+        return Keyword.normalize_text(value)
+
+    def _create_new_keyword(self, value):
+        if ID_VAL_SEPARATOR in value:
+            fast_id, kw_text = value.split(ID_VAL_SEPARATOR, 1)
+            fast_id = fast_id.replace('fst', '')
+            return self.queryset.create(text=kw_text, authority='fast',
+                    authority_uri=FAST_URI, value_uri='%s/%s' % (FAST_URI, fast_id))
+        else:
+            return self.queryset.create(text=value)
 
     def value_from_datadict(self, data, files, field_name):
         values = super(KeywordSelect2TagWidget, self).value_from_datadict(data, files, field_name)
@@ -17,12 +36,12 @@ class KeywordSelect2TagWidget(ModelSelect2TagWidget):
             except ValueError:
                 #val is some text the user just typed
                 #it might be in the db already, just not found because it's composed instead of decomposed
-                search_text = Keyword.normalize_text(val)
+                kw_text = self._get_value_text(val)
                 try:
-                    found_keyword = self.queryset.get(text=search_text)
+                    found_keyword = self.queryset.get(text=kw_text)
                     #we found the keyword, so add this id to cleaned_values
                     cleaned_values.append(found_keyword.id)
                 except Keyword.DoesNotExist:
-                    created_keyword = self.queryset.create(text=val)
+                    created_keyword = self._create_new_keyword(val)
                     cleaned_values.append(created_keyword.id)
         return cleaned_values
