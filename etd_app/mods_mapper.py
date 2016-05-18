55d9d73c31469a95142586d5160e4f898633abe8
Ben Cail <benjamin_cail@brown.edu>
2016-05-18 09:41:36 -0400

    refactor: use self.thesis instead of passing it around

--- etd_app/mods_mapper.py
+++ etd_app/mods_mapper.py
@@ -8,30 +8,30 @@ class ModsMapper(object):
         self.thesis = thesis
 
     def get_mods(self):
-        return self._map_to_mods(self.thesis)
+        return self._map_to_mods()
 
-    def _map_to_mods(self, thesis):
+    def _map_to_mods(self):
         mods_obj = mods.make_mods()
-        mods_obj.title = thesis.title
-        mods_obj = self._add_creator(thesis, mods_obj)
-        mods_obj = self._add_committee(thesis, mods_obj)
-        mods_obj = self._add_department(thesis, mods_obj)
+        mods_obj.title = self.thesis.title
+        mods_obj = self._add_creator(mods_obj)
+        mods_obj = self._add_committee(mods_obj)
+        mods_obj = self._add_department(mods_obj)
         mods_obj.create_origin_info()
-        mods_obj.origin_info.copyright.append(mods.CopyrightDate(date=thesis.candidate.year))
+        mods_obj.origin_info.copyright.append(mods.CopyrightDate(date=self.thesis.candidate.year))
         mods_obj.create_physical_description()
-        mods_obj.physical_description.extent = '%s, %s p.' % (thesis.num_prelim_pages, thesis.num_body_pages)
+        mods_obj.physical_description.extent = '%s, %s p.' % (self.thesis.num_prelim_pages, self.thesis.num_body_pages)
         mods_obj.physical_description.digital_origin = 'born digital'
-        mods_obj.notes.append(mods.Note(text='Thesis (%s -- Brown University %s)' % (thesis.candidate.degree.abbreviation, thesis.candidate.year)))
+        mods_obj.notes.append(mods.Note(text='Thesis (%s -- Brown University %s)' % (self.thesis.candidate.degree.abbreviation, self.thesis.candidate.year)))
         mods_obj.resource_type = 'text'
         mods_obj.genres.append(mods.Genre(text='theses', authority='aat'))
         mods_obj.create_abstract()
-        mods_obj.abstract.text = thesis.abstract
-        mods_obj = self._add_keywords(thesis, mods_obj)
+        mods_obj.abstract.text = self.thesis.abstract
+        mods_obj = self._add_keywords(mods_obj)
         return mods_obj
 
-    def _add_creator(self, thesis, mods_obj):
+    def _add_creator(self, mods_obj):
         n = mods.Name(type='personal')
-        name_text = self._get_name_text(thesis.candidate.person)
+        name_text = self._get_name_text(self.thesis.candidate.person)
         np = mods.NamePart(text=name_text)
         n.name_parts.append(np)
         r = mods.Role(type='text', text='creator')
@@ -45,8 +45,8 @@ class ModsMapper(object):
             name_text += ', %s %s' % (person.first_name, person.middle)
         return name_text.strip()
 
-    def _add_committee(self, thesis, mods_obj):
-        for cm in thesis.candidate.committee_members.all():
+    def _add_committee(self, mods_obj):
+        for cm in self.thesis.candidate.committee_members.all():
             n = mods.Name(type='personal')
             name_text = self._get_name_text(cm.person)
             np = mods.NamePart(text=name_text)
@@ -56,9 +56,9 @@ class ModsMapper(object):
             mods_obj.names.append(n)
         return mods_obj
 
-    def _add_department(self, thesis, mods_obj):
+    def _add_department(self, mods_obj):
         n = mods.Name(type='corporate')
-        name_text = 'Brown University. %s' % thesis.candidate.department.name
+        name_text = 'Brown University. %s' % self.thesis.candidate.department.name
         np = mods.NamePart(text=name_text)
         n.name_parts.append(np)
         r = mods.Role(type='text', text='sponsor')
@@ -66,8 +66,8 @@ class ModsMapper(object):
         mods_obj.names.append(n)
         return mods_obj
 
-    def _add_keywords(self, thesis, mods_obj):
-        for kw in thesis.keywords.all():
+    def _add_keywords(self, mods_obj):
+        for kw in self.thesis.keywords.all():
             subject = mods.Subject(topic=kw.text)
             if kw.authority:
                 subject.authority = kw.authority
