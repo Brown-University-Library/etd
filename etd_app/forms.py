9646d4679296dde6700548b2b635d38a153a5514
Ben Cail <benjamin_cail@brown.edu>
2016-06-27 15:58:45 -0400

    change Keywords label to Topics

--- etd_app/forms.py
+++ etd_app/forms.py
@@ -118,6 +118,7 @@ class MetadataForm(forms.ModelForm):
         model = Thesis
         fields = ['title', 'abstract', 'keywords', 'language', 'num_prelim_pages', 'num_body_pages']
         labels = {
+                'keywords': 'Topics',
                 'num_prelim_pages': 'No. of preliminary pages (roman numerals, e.g. ix)',
                 'num_body_pages': 'No. of pages in dissertation proper (arabic numerals, e.g. 125)',
             }
