557ce6251e2fa9f3c2de5f8d26ac0545254bd3eb
Ben Cail <benjamin_cail@brown.edu>
2016-09-13 10:46:37 -0400

    use 'thesis' in embargo label if it's a masters thesis (also rename a variable)

--- etd_app/forms.py
+++ etd_app/forms.py
@@ -76,7 +76,7 @@ class CandidateForm(forms.ModelForm):
     year = forms.ChoiceField(choices=get_years)
     department = forms.ModelChoiceField(queryset=Department.objects.all().order_by('name'),
             help_text='If your department is missing, please email bdr@brown.edu and we\'ll add it to the list.')
-    set_embargo = forms.BooleanField(label='Restrict access to my dissertation for 2 years (see <a href="https://www.brown.edu/academics/gradschool/dissertation-guidelines">Guidelines</a>)', required=False)
+    set_embargo = forms.BooleanField(required=False)
 
     class Meta:
         model = Candidate
@@ -92,15 +92,18 @@ class CandidateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         degree_type = kwargs.pop('degree_type', '')
         super(CandidateForm, self).__init__(*args, **kwargs)
-        queryset = Degree.objects.all().order_by('name')
+        degrees = Degree.objects.all().order_by('name')
+        embargo_label = 'Restrict access to my %s for 2 years (see <a href="https://www.brown.edu/academics/gradschool/dissertation-guidelines">Guidelines</a>)'
+        self.fields['set_embargo'].label = embargo_label % 'dissertation'
         if degree_type == 'dissertation':
-            queryset = queryset.filter(degree_type=Degree.TYPES.doctorate)
+            degrees = degrees.filter(degree_type=Degree.TYPES.doctorate)
         elif degree_type == 'thesis':
-            queryset = queryset.filter(degree_type=Degree.TYPES.masters)
-        self.fields['degree'] = forms.ModelChoiceField(queryset=queryset, empty_label=None,
-                            widget=forms.RadioSelect(choices=queryset))
-        if len(queryset) == 1:
-            self.fields['degree'].initial = queryset[0]
+            degrees = degrees.filter(degree_type=Degree.TYPES.masters)
+            self.fields['set_embargo'].label = embargo_label % 'thesis'
+        self.fields['degree'] = forms.ModelChoiceField(queryset=degrees, empty_label=None,
+                            widget=forms.RadioSelect(choices=degrees))
+        if len(degrees) == 1:
+            self.fields['degree'].initial = degrees[0]
         self.helper = FormHelper()
         self.helper.label_class = 'col-lg-2'
         self.helper.field_class = 'col-lg-8'
